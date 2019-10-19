64d3452cc27dadf3b9c4f4b2775c595d253a0798
KadeCode15 <49326307+KadeCode15@users.noreply.github.com>
2019-10-19 00:34:26 -0400

    Kade's First Commit

---
+++ Database/DatabaseQueries.cs
@@ -0,0 +1,80 @@
+﻿using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EcoHunt.Database
+{
+    /// <summary>
+    /// This class is meant to be inherited by other classes making use of its methods.
+    /// This contains methods that are used to directly manipulate the data in the database
+    /// </summary>
+    public class DatabaseQueries
+    {
+        protected static IFirebaseConfig config = new FirebaseConfig
+        {
+            AuthSecret = API_Keys.firebaseAuthSecret,
+            BasePath = API_Keys.firebaseBasePath
+        };
+
+        protected static IFirebaseClient client;
+
+        public static bool IsClientConnected = false;
+
+        /// <summary>
+        /// This creates the connection to the Firebase Database
+        /// It returns true if it succeeds
+        /// </summary>
+        /// <returns></returns>
+        public static bool ConnectToDataBase()
+        {
+            client = new FireSharp.FirebaseClient(config);
+            if (client != null)
+            {
+                IsClientConnected = true;
+            }
+            else
+            {
+                //OH CRAP!!
+                IsClientConnected = false;
+            }
+            return IsClientConnected;
+        }
+
+
+        protected static void CheckForConnection()
+        {
+            if (IsClientConnected)
+                return;
+            else
+            {
+                bool didSucceed = ConnectToDataBase();
+                if (!didSucceed)
+                    throw new Exception("Client is not connected. Attempted: Could not connect.");
+            }
+        }
+
+        protected static FirebaseResponse UpdateData(string path, object data)
+        {
+            FirebaseResponse response = client.Update(path, data);
+            return response;
+        }
+        protected static void DeleteData(string path)
+        {
+            FirebaseResponse response = client.Delete(path);
+        }
+        protected static FirebaseResponse GetData(string path)
+        {
+            FirebaseResponse response = client.Get(path);
+            return response;
+        }
+        protected static SetResponse InsertData(string path, object item)
+        {
+            SetResponse response = client.Set(path, item);
+            return response;
+        }
+    }
+}
---
+++ Database/FirebaseDatabase.cs
@@ -0,0 +1,83 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EcoHunt.Database
+{
+    public class FirebaseDatabase : DatabaseQueries
+    {
+        public static NamesValues[] GetAllPictureNames()
+        {
+            CheckForConnection();
+            var response = GetData("Names");
+            object json = JsonConvert.DeserializeObject(response.Body);
+
+            List<NamesValues> patchNotes = new List<NamesValues>();
+            foreach (JToken item in ((JToken)(json)).Children())
+            {
+                var newPatchNote = item.ToObject<NamesValues>();
+                patchNotes.Add(newPatchNote);
+            }
+
+            for(int x = 0; x < patchNotes.Count(); x++)
+            {
+                if (patchNotes[x] == null)
+                    patchNotes.RemoveAt(x);
+            }
+
+            return patchNotes.ToArray();
+        }
+        public static void DeletePicture(string pictureName)
+        {
+            CheckForConnection();
+            var allNames = GetAllPictureNames();
+            for(int x = 0; x < allNames.Length; x++)
+            {
+                if(allNames[x].name == pictureName)
+                {
+                    DeletePicture(allNames[x].ID);
+                    break;
+                }
+            }
+        }
+        public static void DeletePicture(int pictureIndex)
+        {
+            CheckForConnection();
+            int currentNum = GetNumberOfNames();
+
+            DeleteData("Names/" + pictureIndex);
+
+            if (currentNum == pictureIndex)
+                SetNumberOfNames(GetAllPictureNames().Last().ID);
+        }
+
+        public static int GetNumberOfNames()
+        {
+            CheckForConnection();
+            return Convert.ToInt32(client.Get("NumberOfNames").Body);
+        }
+        private static void SetNumberOfNames(int number)
+        {
+            CheckForConnection();
+            InsertData("NumberOfNames", number);
+        }
+        public static void AddPicture(string picName)
+        {
+            CheckForConnection();
+            int newID = GetNumberOfNames() + 1;
+
+            NamesValues stuffToInput = new NamesValues { ID = newID, name = picName };
+
+            InsertData("Names/" + stuffToInput.ID, stuffToInput);
+            SetNumberOfNames(newID);
+        }
+    }
+    public class NamesValues
+    {
+        public string name;
+        public int ID;
+    }
+}
