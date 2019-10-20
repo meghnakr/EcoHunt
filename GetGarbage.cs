a0a23ee355da427b6de74e1d57cd1d64d97a44b4
KadeCode15 <49326307+KadeCode15@users.noreply.github.com>
2019-10-19 23:12:30 -0400

    Finished first working prototype

---
+++ GetGarbage.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Azure.CognitiveServices.Vision.CustomVision.Prediction;
+using Microsoft.Azure.CognitiveServices.Vision.CustomVision.Training;
+using Microsoft.Azure.CognitiveServices.Vision.CustomVision.Training.Models;
+
+namespace EcoHunt
+{
+    class GetGarbage
+    {
+        public static bool CheckGarbage(String link) //Give a link to image file
+        {
+            string predictionKey = EcoHunt.API_Keys.AzurePredictionKey;
+            string endpoint = EcoHunt.API_Keys.AzureEndpoint;
+            string name = EcoHunt.API_Keys.AzurepublishedName;
+            string projectId = EcoHunt.API_Keys.AzureProjectID;
+
+            CustomVisionPredictionClient predictionAPI = new CustomVisionPredictionClient()
+            {
+                ApiKey = predictionKey,
+                Endpoint = endpoint
+            };
+
+            var uri = new Uri(link);
+            var imageUrl = new Microsoft.Azure.CognitiveServices.Vision.CustomVision.Prediction.Models.ImageUrl(link);
+            var result = predictionAPI.ClassifyImageUrl(new Guid(projectId), name, imageUrl);
+            var s = result.Predictions;
+            var x = s[0].TagName;
+            // Loop over each prediction and write out the results
+            if (result.Predictions[0].TagName.Equals("Garbage"))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
